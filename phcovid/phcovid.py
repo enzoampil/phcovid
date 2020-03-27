e15829b43d21969db32bf22d133c1f6d69b1747b
amendoza <amendoza@stratpoint.com>
2020-03-27 19:59:40 +0800

    feat(phcovid.py): moved constants to a separate file
    Suggesting to segragate data from the actual logic of code. This allows contributions to be made to where they are (be it for data changes or code changes).

--- phcovid/phcovid.py
+++ phcovid/phcovid.py
@@ -4,31 +4,10 @@ from pandas.io.json import json_normalize
 import re
 import pandas as pd
 
-URL = "https://services5.arcgis.com/mnYJ21GiFTR97WFg/arcgis/rest/services/PH_masterlist/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=%2A&outSR=102100&cacheHint=true&fbclid=IwAR1nboPFyAVQ5sgE0QmE31B9ZqveQcB3tUAlegIqUoqkV8057zWGHF2RsRU"
-RENAME_DICT = {
-    "attributes.PH_masterl": "case_no",
-    "attributes.edad": "age",
-    "attributes.kasarian": "sex",
-    "attributes.nationalit": "nationality",
-    "attributes.residence": "residence",
-    "attributes.travel_hx": "travel_history",
-    "attributes.symptoms": "symptoms",
-    "attributes.confirmed": "confirmation_date",
-    "attributes.facility": "facility",
-    "attributes.latitude": "latitude",
-    "attributes.longitude": "longitude",
-    "attributes.status": "status",
-    "attributes.epi_link": "epi_link",
-    "attributes.petsa": "date",
-}
-
-VAL_ALIAS = [
-    "For validation",
-    "for validation",
-    "for validation",
-    "For Validation",
-]  # ,
-NONE_ALIAS = ["none", ""]
+from .constants import URL
+from .constants import NONE_ALIAS
+from .constants import VAL_ALIAS
+from .constants import RENAME_DICT
 
 
 def extract_contact_info(travel_history):
