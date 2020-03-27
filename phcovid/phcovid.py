f2abea298fb0fb41cf460d8399e9b964ed15f7f4
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-03-27 08:03:17 +0800

    Format with black

--- phcovid/phcovid.py
+++ phcovid/phcovid.py
@@ -4,26 +4,32 @@ from pandas.io.json import json_normalize
 import re
 import pandas as pd
 
-URL = 'https://services5.arcgis.com/mnYJ21GiFTR97WFg/arcgis/rest/services/PH_masterlist/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=%2A&outSR=102100&cacheHint=true&fbclid=IwAR1nboPFyAVQ5sgE0QmE31B9ZqveQcB3tUAlegIqUoqkV8057zWGHF2RsRU'
+URL = "https://services5.arcgis.com/mnYJ21GiFTR97WFg/arcgis/rest/services/PH_masterlist/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=%2A&outSR=102100&cacheHint=true&fbclid=IwAR1nboPFyAVQ5sgE0QmE31B9ZqveQcB3tUAlegIqUoqkV8057zWGHF2RsRU"
 RENAME_DICT = {
-    'attributes.PH_masterl': 'case_no',
-    'attributes.edad': 'age',
-    'attributes.kasarian': 'sex',
-    'attributes.nationalit': 'nationality',
-    'attributes.residence': 'residence',
-    'attributes.travel_hx': 'travel_history',
-    'attributes.symptoms': 'symptoms',
-    'attributes.confirmed': 'confirmation_date',
-    'attributes.facility': 'facility',
-    'attributes.latitude': 'latitude',
-    'attributes.longitude': 'longitude',
-    'attributes.status': 'status',
-    'attributes.epi_link': 'epi_link',
-    'attributes.petsa': 'date',
+    "attributes.PH_masterl": "case_no",
+    "attributes.edad": "age",
+    "attributes.kasarian": "sex",
+    "attributes.nationalit": "nationality",
+    "attributes.residence": "residence",
+    "attributes.travel_hx": "travel_history",
+    "attributes.symptoms": "symptoms",
+    "attributes.confirmed": "confirmation_date",
+    "attributes.facility": "facility",
+    "attributes.latitude": "latitude",
+    "attributes.longitude": "longitude",
+    "attributes.status": "status",
+    "attributes.epi_link": "epi_link",
+    "attributes.petsa": "date",
 }
 
-VAL_ALIAS = ['For validation', 'for validation', 'for validation', 'For Validation'] #, 
-NONE_ALIAS = ['none', '']
+VAL_ALIAS = [
+    "For validation",
+    "for validation",
+    "for validation",
+    "For Validation",
+]  # ,
+NONE_ALIAS = ["none", ""]
+
 
 def extract_contact_info(travel_history):
     """
@@ -31,12 +37,13 @@ def extract_contact_info(travel_history):
     travel_history:
         Iterable from which to parse contacts of the form PHX ('travel_history' column from get_cases())
     """
-    def get_contacts (s):
+
+    def get_contacts(s):
         try:
-            return re.findall(r'\bPH\w+',s)
+            return re.findall(r"\bPH\w+", s)
         except TypeError:
             return ""
-    
+
     contacts = []
     num_contacts = []
     for case in travel_history:
@@ -44,19 +51,27 @@ def extract_contact_info(travel_history):
         contacts.append(contacts_)
         num_contacts.append(len(contacts_))
 
-    contact_info = pd.DataFrame({'contacts': contacts, 'num_contacts': num_contacts})
+    contact_info = pd.DataFrame({"contacts": contacts, "num_contacts": num_contacts})
     return contact_info
 
-def get_cases(url=URL, rename_dict=RENAME_DICT, val_alias=VAL_ALIAS, none_alias=NONE_ALIAS):
+
+def get_cases(
+    url=URL, rename_dict=RENAME_DICT, val_alias=VAL_ALIAS, none_alias=NONE_ALIAS
+):
     """
     Returns cleaned data from DOH COVID for PH (https://www.facebook.com/notes/wilson-chua/working-with-doh-covid-data/2868993263159446/)
     """
     raw = json.loads(urlopen(url).read())
-    df = json_normalize(raw['features'])
+    df = json_normalize(raw["features"])
     df_renamed = df[rename_dict.keys()].rename(columns=rename_dict)
-    df_aliased = df_renamed.replace(val_alias, 'for_validation').replace(none_alias, 'none')
-    df_aliased[['contacts', 'num_contacts']] = extract_contact_info(df_aliased.travel_history)
+    df_aliased = df_renamed.replace(val_alias, "for_validation").replace(
+        none_alias, "none"
+    )
+    df_aliased[["contacts", "num_contacts"]] = extract_contact_info(
+        df_aliased.travel_history
+    )
     return df_aliased
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(get_cases().head())
--- phcovid/phcovid_plot.py
+++ phcovid/phcovid_plot.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def get_case_plot(df, date_col = 'confirmation_date'):
+
+def get_case_plot(df, date_col="confirmation_date"):
     """
     This function a.) plots the growth of cases in the given df
     and prints the date of first and latest confirmed case.
@@ -16,8 +17,8 @@ def get_case_plot(df, date_col = 'confirmation_date'):
     """
 
     def get_date_part(d):
-        #Function removes empty/invalid dates
-        #Once data cleaning in get_cases() is finalized, we can depracate this fucntion and section
+        # Function removes empty/invalid dates
+        # Once data cleaning in get_cases() is finalized, we can depracate this fucntion and section
         try:
             date = pd.to_datetime(d)
             return d
@@ -27,26 +28,32 @@ def get_case_plot(df, date_col = 'confirmation_date'):
     case_dates = df[date_col]
     case_dates = case_dates.apply(lambda x: get_date_part(x))
     case_count = pd.DataFrame(case_dates[~case_dates.isnull()].unique())
-    case_count.rename(columns = {0: 'date'}, inplace = True)
-    
+    case_count.rename(columns={0: "date"}, inplace=True)
+
     for row in case_count.itertuples():
-        current_date = getattr(row, 'date')
-        index = getattr(row, 'Index')
-        
-        #column 'case_count' stores number of cases in that specific date. 
-        case_count.loc[index, 'case_count'] = len(df[df[date_col]==current_date])
+        current_date = getattr(row, "date")
+        index = getattr(row, "Index")
 
-        #create cumulative sum of case_count
-        if(index == 0):
-            case_count.loc[index, 'case_count'] = 0
+        # column 'case_count' stores number of cases in that specific date.
+        case_count.loc[index, "case_count"] = len(df[df[date_col] == current_date])
+
+        # create cumulative sum of case_count
+        if index == 0:
+            case_count.loc[index, "case_count"] = 0
         else:
-            case_count.loc[index, 'case_count'] += case_count.loc[index-1, 'case_count']
+            case_count.loc[index, "case_count"] += case_count.loc[
+                index - 1, "case_count"
+            ]
 
-        
-    
-    #print first and latest confirmed date
-    print("First Confirmed Case: ", str(min(pd.to_datetime(case_count['date']))).split(' ')[0])
-    print("Latest Confirmed Case: ", str(max(pd.to_datetime(case_count['date']))).split(' ')[0])
-    
-    #plot
-    plt.plot(case_count['date'], case_count['case_count'])
+    # print first and latest confirmed date
+    print(
+        "First Confirmed Case: ",
+        str(min(pd.to_datetime(case_count["date"]))).split(" ")[0],
+    )
+    print(
+        "Latest Confirmed Case: ",
+        str(max(pd.to_datetime(case_count["date"]))).split(" ")[0],
+    )
+
+    # plot
+    plt.plot(case_count["date"], case_count["case_count"])
