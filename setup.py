728e3f785a5feb8415025691fa6a0be36b152e38
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-03-27 08:45:32 +0800

    Add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+  
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    install_requires = fh.read().splitlines()
+
+setuptools.setup(
+     name='phcovid',  
+     version='0.0.1',
+     author="Lorenzo Ampil",
+     author_email="lorenzo.ampil@gmail.com",
+     description="Get PH COVID data in only two lines of code!",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/enzoampil/phcovid",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+     install_requires=install_requires,
+ )
