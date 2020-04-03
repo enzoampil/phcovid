50048eda8aa69a01fd6b9eab77a29297a4f45fe0
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-04-03 08:06:11 +0800

    Change arcgis url parameters to relax 2k constraint

--- phcovid/data_extractor/arcgis.py
+++ phcovid/data_extractor/arcgis.py
@@ -1,16 +1,4 @@
-URL = (
-    "https://services5.arcgis.com"
-    "/mnYJ21GiFTR97WFg/arcgis/rest/services"
-    "/PH_masterlist/FeatureServer/0/query"
-    # url query params
-    "?f=json&where=1%3D1"
-    "&returnGeometry=true"
-    "&spatialRel=esriSpatialRelIntersects"
-    "&outFields=%2A"
-    "&outSR=102100"
-    "&cacheHint=true"
-    "&fbclid=IwAR1nboPFyAVQ5sgE0QmE31B9ZqveQcB3tUAlegIqUoqkV8057zWGHF2RsRU"
-)
+URL = "https://services5.arcgis.com/mnYJ21GiFTR97WFg/arcgis/rest/services/PH_masterlist/FeatureServer/0/query?f=json&where=1%3D1&resultType=standard&spatialRel=esriSpatialRelIntersects&outFields=%2A&outSR=102100&cacheHint=true&fbclid=IwAR1GLKbsmvS3ha9Hod61ZVgnApA2VW9dy9-RYwnYwJUYUKf97Oxj5EeVBCU"
 
 
 def extract_arcgis_data():
